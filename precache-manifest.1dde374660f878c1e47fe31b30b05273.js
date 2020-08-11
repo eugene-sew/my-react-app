392514b033a835f4e63c1c0f5bd2e3f681bb6087
black-hawk <eugene-sew>
2020-08-11 16:47:25 +0000

    Updates

---
+++ precache-manifest.1dde374660f878c1e47fe31b30b05273.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1be2c2cf0b8e683d282fb715195f37c",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "a9b35cc092d07c035d1d",
+    "url": "/my-react-app/static/css/main.f84e3ef8.chunk.css"
+  },
+  {
+    "revision": "0dfcd0fb39aec46af27e",
+    "url": "/my-react-app/static/js/2.ed278bc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-app/static/js/2.ed278bc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b35cc092d07c035d1d",
+    "url": "/my-react-app/static/js/main.f07e1bdc.chunk.js"
+  },
+  {
+    "revision": "435064591b6543e8ca17",
+    "url": "/my-react-app/static/js/runtime-main.c0584337.js"
+  },
+  {
+    "revision": "f1371a648f4932fc4ec5c50355534fff",
+    "url": "/my-react-app/static/media/card1.f1371a64.png"
+  },
+  {
+    "revision": "9910efa2ede1f3efd57a24e808c7cd00",
+    "url": "/my-react-app/static/media/card2.9910efa2.png"
+  },
+  {
+    "revision": "22ac71deeef138cf6d9520364d73d03d",
+    "url": "/my-react-app/static/media/card3.22ac71de.jpg"
+  }
+]);
